0b4b17fdea7115ee1598d54057e96b08447ee136
yjy <yjy11mm@126.com>
2021-07-12 16:47:46 +0800

    2

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `age` tinyint(3) unsigned NOT NULLsunnnnnnnnnnnnnn,
-  `email` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL COMMENT '123',
   `avatar` varchar(100) DEFAULT NULL COMMENT '头像地址11sunXXXXXXXX',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
